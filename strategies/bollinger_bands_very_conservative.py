1b1815ecf70487021c565b0aad80ab1cbc6258bc
aristosv <aristos@aristos.net>
2021-05-17 13:34:38 +0300

    Update bollinger_bands_very_conservative.py

--- strategies/bollinger_bands_very_conservative.py
+++ strategies/bollinger_bands_very_conservative.py
@@ -4,7 +4,7 @@ from freqtrade.strategy.interface import IStrategy
 from typing import Dict, List
 from functools import reduce
 import talib.abstract as ta
-class $class(IStrategy):
+class freqtrade(IStrategy):
 
     stoploss = -0.4
     timeframe = '1h'
@@ -14,6 +14,7 @@ class $class(IStrategy):
     trailing_only_offset_is_reached = True
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['rsi'] = ta.RSI(dataframe)
         bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=3)
         dataframe['bb_lowerband'] = bollinger['lower']
         dataframe['bb_middleband'] = bollinger['mid']
